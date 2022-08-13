8285a172d7bb4449bc53313b6a6d7164661e83ab
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-08-12 22:20:22 -0600

    feat: add cases

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ name_of_unit = "seconds"
 def days_to_units(days):
     if days > 0:
         return f"{days} days are {(days * calculation_to_seconds)} {name_of_unit}"
-    else:
-        return "you entered a negative value, so no conversion for you!"
+    elif days == 0:
+        return "you entered a 0, please enter a valid positive number"
 
 
 print(days_to_units(22))
 print(days_to_units(30))
 print(days_to_units(40))
 
-days = int(input("hey user, enter a number of days and I will convert it to hours!\n"))
+user_input = input("hey user, enter a number of days and I will convert it to hours!\n")
+if user_input.isdigit():
+    days = int(user_input)
+    print(days_to_units(days))
+else:
+    print('sorry it is not working')
 
-days_to_units(days)
